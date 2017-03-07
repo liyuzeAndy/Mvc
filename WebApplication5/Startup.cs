7dedb4437ad0eda2e6884e13135667fe7ca513e8
liyuzeAndy <707305576@qq.com>
2017-03-07 10:11:24 +0800

    Update Startup.cs

--- WebApplication5/Startup.cs
+++ WebApplication5/Startup.cs
@@ -8,7 +8,7 @@ namespace WebApplication5
     {
         public void Configuration(IAppBuilder app)
         {
-            //adwdaw
+            //adwdawdawdawdawdaw11111
             ConfigureAuth(app);
         }
     }
